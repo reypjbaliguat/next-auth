d604e33d6ea1f728f0e9fab1269c9c49c45a43c4
reypjbaliguat <reypjbaliguat@gmail.com>
2024-07-14 19:56:13 +0800

    login and signout added

---
+++ auth.ts
@@ -0,0 +1,15 @@
+import authConfig from "@/auth.config";
+import { db } from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+
+export const {
+  auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
